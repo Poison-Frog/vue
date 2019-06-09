3f93b44d19163d32d86a5c05db23d97d852d376e
Poison-Frog <46836317+Poison-Frog@users.noreply.github.com>
2019-06-09 10:14:15 +0800

    更新代码

--- sell/.eslintrc.js
+++ sell/.eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'semi':['error','always']
   }
 }
