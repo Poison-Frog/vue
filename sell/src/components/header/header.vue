fb6dec5cb2ca0465724ff410ba45b6262eb66291
Poison-Frog <46836317+Poison-Frog@users.noreply.github.com>
2019-06-22 01:37:10 +0800

    更新代码

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -17,8 +17,26 @@
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
+			<div v-if="seller.supports" class="support-count" @click="showDetail">
+				<span class="count">{{seller.supports.length}}个</span>
+				<i class="icon-keyboard_arrow_right"></i>
+			</div>
 		</div>
-		<div class="bulletin-wrapper"></div>
+		<div class="bulletin-wrapper" @click="showDetail">
+			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+			<i class="icon-keyboard_arrow_right"></i>
+		</div>
+		<div class="background">
+			<img :src="seller.avatar" width="100%" height="100%">
+		</div>
+		<div v-show="detailShow" class="detail">
+			<div class="detail-wrapper clearfix">
+				<div class="detail-main"></div>
+			</div>
+			<div class="detail-close">
+				<i class="icon-close"></i>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -27,6 +45,16 @@
 		props: {
 			seller: {
 				type: Object
+			}
+		},
+		data() {
+			return {
+				detailShow: false
+			};
+		},
+		methods: {
+			showDetail() {
+				this.detailShow = true;
 			}
 		},
 		created() {
@@ -39,10 +67,13 @@
 	@import "../../common/stylus/mixin";
 
 	.header
+		position: relative
 		color: #fff
-		background: #000
+		background: rgba(7,17,27,0.5)
 		.content-wrapper
+			position: relative
 			padding: 24px 12px 18px 24px
+			font-size: 0
 			.avatar
 				display: inline-block
 				vertical-align: top
@@ -73,7 +104,7 @@
 				.support
 					.icon
 						display: inline-block
-						vertical-align: middle
+						vertical-align: top
 						width: 12px
 						height: 12px
 						margin-right: 4px
@@ -91,5 +122,78 @@
 							bg-image('special_1')
 					.text
 						line-height: 12px
-						font-size: 12px
+						font-size: 10px
+			.support-count
+				position: absolute
+				right: 12px
+				bottom: 14px
+				padding: 0 8px
+				height: 24px
+				line-height: 24px
+				border-radius: 14px
+				background: rgba(0,0,0,0.2)
+				text-align: center
+				.count
+					vertical-align: top
+					font-size: 10px
+				.icon-keyboard_arrow_right
+					margin-left: 2px
+					line-height: 24px
+					font-size: 10px
+		.bulletin-wrapper
+			position: relative
+			height: 28px
+			line-height: 28px
+			padding: 0 22px 0 12px
+			white-space: nowrap
+			overflow: hidden
+			text-overflow: ellipsis
+			background: rgba(7,17,27,0.2)
+			.bulletin-title
+				display: inline-block
+				vertical-align: top
+				margin-top: 8px
+				width: 22px
+				height: 12px
+				bg-image('bulletin')
+				background-size: 22px 12px
+				background-repeat: no-repeat
+			.bulletin-text
+				vertical-align: top
+				margin: 0 4px
+				font-size: 10px
+			.icon-keyboard_arrow_right
+				position: absolute
+				font-size: 10px
+				right: 12px
+				top: 8px
+		.background
+			position: absolute
+			top: 0
+			left: 0
+			width: 100%
+			height: 100%
+			z-index: -1
+			filter: blur(10px)
+		.detail
+			position: fixed
+			z-index: 100
+			top: 0
+			left: 0
+			width: 100%
+			height: 100%
+			overflow: auto
+			background: rgba(7,17,27,0.8)
+			.detail-wrapper
+				min-height: 100%
+				.detail-main
+					margin-top: 64px
+					padding-bottom: 64px
+			.detail-close
+				position: relative
+				width: 32px
+				height: 32px
+				margin: -64px auto 0 auto
+				clear: both
+				font-size: 32px
 </style>
