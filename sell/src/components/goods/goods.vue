8d1c64f33a5f31c82afcf6b87a0c5a3e969c7755
Poison-Frog <46836317+Poison-Frog@users.noreply.github.com>
2019-06-27 04:19:58 +0800

    更新代码

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -2,7 +2,7 @@
 	<div class="goods">
 		<div class="menu-wrapper" ref="menuWrapper">
 			<ul>
-				<li v-for="(item,index) in goods" :key="index" class="menu-item" :class="{'current':currentIndex===index}">
+				<li v-for="(item,index) in goods" :key="index" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index,$event)">
 					<span class="text border-1px">
 						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
 					</span>
@@ -11,7 +11,7 @@
 		</div>
 		<div class="foods-wrapper" ref="foodsWrapper">
 			<ul>
-				<li v-for="(item,index) in goods" :key="index" class="food-list food-list-hook">
+				<li v-for="(item,index) in goods" :key="index" class="food-list" ref="foodList">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
 						<li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
@@ -34,11 +34,13 @@
 				</li>
 			</ul>
 		</div>
+		<shopcart></shopcart>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
 	import BScroll from 'better-scroll';
+	import shopcart from '../shopcart/shopcart.vue';
 
 	const ERROR_OK = 0;
 
@@ -60,7 +62,7 @@
 				for (let i = 0; i < this.listHeight.length; i++) {
 					let height1 = this.listHeight[i];
 					let height2 = this.listHeight[i + 1];
-					if (!height2 || (this.scrollY > height1 && this.scrollY < height2)) {
+					if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
 						return i;
 					}
 				}
@@ -82,14 +84,25 @@
 			});
 		},
 		methods: {
+			selectMenu(index, event) {
+				if (!event._constructed) {
+					return;
+				}
+				let foodList = this.$refs.foodList;
+				let el = foodList[index];
+				this.foodsScroll.scrollToElement(el, 300);
+				console.log(index);
+			},
 			_initScroll() {
-				this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+				this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+					click: true
+				});
 
 				this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
 					probeType: 3
 				});
 
-				this.foodScroll.on('scroll', (pos) => {
+				this.foodsScroll.on('scroll', (pos) => {
 					if (pos.y <= 0) {
 						this.scrollY = Math.abs(Math.round(pos.y));
 					}
@@ -105,6 +118,9 @@
 					this.listHeight.push(height);
 				}
 			}
+		},
+		components: {
+			shopcart
 		}
 	};
 </script>
